f68182b0fa2a603fc85c45abf7ec581d78720c82
Han Fang <hfang8@crimson.ua.edu>
2019-09-29 22:52:09 -0500

    Add nedb to server

--- app/server/main.js
+++ app/server/main.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const nedb = require("nedb");
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
+const userProfileDB = new nedb({ filename: "./userProfile.db", autoload: true });
+const userDataDB = new nedb({ filename: "./userData.db", autoload: true });
+const qualtricDB = new nedb({ filename: "./qualtric.db", autoload: true });
+
 var app = express();
 const port = 8888;
-
-
 
 app.use(express.json());
 app.post('/login', function (req, res) {
@@ -18,4 +21,4 @@ app.post('/login', function (req, res) {
     }));
 })
 
-app.listen(port, () => console.log(`Backend listening on port ${port}!`))
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
