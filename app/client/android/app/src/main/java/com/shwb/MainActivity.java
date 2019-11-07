c4dee39648e223569e10879395e0bc0ef5d570f4
bdgerszewski <bdgerszewski@crimson.ua.edu>
2019-11-07 16:34:35 -0600

    Added stuff to get drawer to behave properly, working on fixing drawer items

--- app/client/android/app/src/main/java/com/shwb/MainActivity.java
+++ app/client/android/app/src/main/java/com/shwb/MainActivity.java
@@ -2,6 +2,12 @@ package com.shwb;
 
 import com.facebook.react.ReactActivity;
 
+// added these three imports below
+
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -11,5 +17,16 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "SHWB";
+    }
+
+    // added override below
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+    };
     }
 }
