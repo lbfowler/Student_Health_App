b25ba2f6f5eede155cbb4b1e0c04336f6fbcd6bf
unknown <hfang8@crimson.ua.edu>
2019-11-19 05:52:35 -0600

    Created three test cases for backend

---
+++ app/server/test/api-test.js
@@ -0,0 +1,33 @@
+const expect = require('chai').expect;
+const request = require('request');
+
+
+it('Login Test', function (done) {
+    request('http://localhost:8888/login', {method: 'POST'}, function (error, response, body) {
+        expect(body).to.not.equal(undefined);
+        var jsonObj = JSON.parse(body);
+        expect(jsonObj.success).to.not.equal(undefined);
+        expect(jsonObj.success).to.equal(false);
+        done();
+    })
+})
+
+it('Register Test', function (done) {
+    request('http://localhost:8888/register', {method: 'POST'}, function (error, response, body) {
+        expect(body).to.not.equal(undefined);
+        var jsonObj = JSON.parse(body);
+        expect(jsonObj.success).to.not.equal(undefined);
+        expect(jsonObj.success).to.equal(false);
+        done();
+    })
+})
+
+it('Unauthorized Test', function (done) {
+    request('http://localhost:8888/api/getAllQuestions', {method: 'GET'}, function (error, response, body) {
+        expect(body).to.not.equal(undefined);
+        var jsonObj = JSON.parse(body);
+        expect(jsonObj.success).to.not.equal(undefined);
+        expect(jsonObj.success).to.equal(false);
+        done();
+    })
+})
